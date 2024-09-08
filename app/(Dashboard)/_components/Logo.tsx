3992e6c98810ed84192cbc852a28a0ff2167efdc
Parag0712 <paragvadgama123@gmail.com>
2024-09-08 21:49:18 +0530

    logo png added

--- app/(Dashboard)/_components/Logo.tsx
+++ app/(Dashboard)/_components/Logo.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-x-2">
-      <Image src="/next.svg" alt="logo" width={130} height={130} />
+    <div className="flex items-center justify-center   gap-x-2">
+      <Image src="/logo.png" alt="logo" width={240} height={100} />
     </div>
   );
 };
