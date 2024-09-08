5041e849d8e50760bd406902845219b2d75e6505
Parag0712 <paragvadgama123@gmail.com>
2024-09-09 02:40:10 +0530

    role base system added

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
+import { clerkClient, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoutes = createRouteMatcher([
@@ -6,17 +7,35 @@ const isPublicRoutes = createRouteMatcher([
   "/sign-up",
   "/api/uploadthing",
 ]);
+export default clerkMiddleware(async (auth, req) => {
+  const data = auth();
+  // Fetch the user details from Clerk backend using the userId
+  let user;
+  if (data.userId) {
+    try {
+      user = await clerkClient.users.getUser(data.userId); // Correct method is `getUser`
+      console.log(user.publicMetadata.role);
+      await clerkClient.users.updateUser(data.userId, {
+        publicMetadata: {
+          role:user.publicMetadata.role
+        }
+      });
+      if(user.publicMetadata.role != "instructor" && req.nextUrl.pathname.startsWith("/teacher")) {
+        return NextResponse.redirect("/");
+      }
+    } catch (error) {
+      console.error("Error fetching user: ", error);
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
 
-export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
-  
   // If it's an UploadThing request or a public route, allow it
   if (isPublicRoutes(req)) {
     return NextResponse.next();
   }
 
   // If user is not logged in and trying to access a protected route
-  if (!userId) {
+  if (!data.userId) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
