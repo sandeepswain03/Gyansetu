cec004f88159c0a22929cff910e02dee6e689a2d
SandeepSwain <sandeepswain2004@gmail.com>
2024-09-08 09:01:49 +0530

    Chapter publish actions

--- app/(Dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(Dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,7 +9,8 @@ import { ChapterVideoForm } from "../_components/ChapterVideoForm";
 import ChapterTitleForm from "../_components/ChapterTitleForm";
 import ChapterDescriptionForm from "../_components/ChapterDescription";
 import ChapterAccessForm from "../_components/ChapterAccessForm";
-
+import { ChapterActions } from "../_components/ChapterActions";
+import { Banner } from "@/components/banner";
 async function page({
   params,
 }: {
@@ -40,75 +41,91 @@ async function page({
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
   const completionText = `(${completedFields} / ${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean)
   return (
-    <div className="p-6">
-      {/* Completion */}
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="mr-2 h-4 w-4" />
-            Back to course setup
-          </Link>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">Chapter Creation</h1>
-              <span className="text-sm text-slate-700">
-                Complete all fields {completionText}
-              </span>
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="This chapter is unpublished. It will not be visible on the course."
+        />
+      )}
+      <div className="p-6">
+        {/* Completion */}
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="mr-2 h-4 w-4" />
+              Back to course setup
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Chapter Creation</h1>
+                <span className="text-sm text-slate-700">
+                  Complete all fields {completionText}
+                </span>
+              </div>
+              <ChapterActions
+              disabled={!isComplete}
+              courseId={courseId}
+              chapterId={chapterId}
+              isPublished={chapter.isPublished}
+              />
             </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          {/* Left side */}
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Customize your chapter</h2>
+              </div>
+              {/* Chapter title form */}
+              <ChapterTitleForm
+                initialData={chapter}
+                courseId={courseId}
+                chapterId={chapterId}
+              />
+              {/* Chapter description form */}
+              <ChapterDescriptionForm
+                initialData={chapter}
+                courseId={courseId}
+                chapterId={chapterId}
+              />
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Eye} />
+                <h2 className="text-xl">Access Settings</h2>
+              </div>
+              <ChapterAccessForm
+                initialData={chapter}
+                courseId={courseId}
+                chapterId={chapterId}
+              />
+            </div>
+          </div>
+          {/* Right side */}
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Video} />
+              <h2 className="text-xl">Add a video</h2>
+            </div>
+            <ChapterVideoForm
+              initialData={chapter}
+              courseId={courseId}
+              chapterId={chapterId}
+            />
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        {/* Left side */}
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">Customize your chapter</h2>
-            </div>
-            {/* Chapter title form */}
-            <ChapterTitleForm
-              initialData={chapter}
-              courseId={courseId}
-              chapterId={chapterId}
-            />
-            {/* Chapter description form */}
-            <ChapterDescriptionForm
-              initialData={chapter}
-              courseId={courseId}
-              chapterId={chapterId}
-            />
-          </div>
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={Eye} />
-              <h2 className="text-xl">Access Settings</h2>
-            </div>
-            <ChapterAccessForm
-              initialData={chapter}
-              courseId={courseId}
-              chapterId={chapterId}
-            />
-          </div>
-        </div>
-        {/* Right side */}
-        <div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={Video} />
-            <h2 className="text-xl">Add a video</h2>
-          </div>
-          <ChapterVideoForm
-            initialData={chapter}
-            courseId={courseId}
-            chapterId={chapterId}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,71 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("[CHAPTER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
@@ -13,7 +78,7 @@ export async function PATCH(
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
-    
+
     const courseOwner = await db.course.findUnique({
       where: {
         id: courseId,
