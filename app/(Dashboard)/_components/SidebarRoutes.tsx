0131dd27394359d2fed98e535d2c4ae33ae2def8
Parag07 <118881644+Parag0712@users.noreply.github.com>
2024-12-07 22:34:50 +0530

    removr comminity icon

--- app/(Dashboard)/_components/SidebarRoutes.tsx
+++ app/(Dashboard)/_components/SidebarRoutes.tsx
@@ -14,11 +14,6 @@ const guestRoutes = [
     icon: Compass,
     label: "Browse",
     href: "/search",
-  },
-  {
-    icon: UserRoundSearch,
-    label: "Community",
-    href: "/community",
   }
 ];
 
